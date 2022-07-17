4ff4878c4dd48e2751133f9b9ec726fae2aa2f04
ccwssb <103161388+ccwssb@users.noreply.github.com>
2022-07-17 09:40:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+git clone https://github.com/kenzok8/small-package package/small-package
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/KimJungWha/default-settings package/default-settings
